3d75606383ba13508455b9e8f0c68bcea908bc0f
Jitu <jitendarsingh04@gmail.com>
2025-07-05 13:28:26 +0530

    deleted

--- Practise_24_06_25/Child_module/Database/main.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_mssql_database" "jitudb" {
- name                = var.database_name
-  sku_name            = var.sku_name
-  collation           = var.collation
-  server_id           = data.azurerm_mssql_server.jituserver.id
-
-  
-}
